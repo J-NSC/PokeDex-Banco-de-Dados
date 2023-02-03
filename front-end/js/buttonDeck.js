9617abea601a0b19cf9aa2fa14af48d4f8e303bf
jhonicley <jhonisilva5002gmail.com>
2023-02-03 12:19:18 -0300

    Botão de remover criado

--- front-end/js/buttonDeck.js
+++ front-end/js/buttonDeck.js
@@ -7,7 +7,9 @@ const imgDeck = document.querySelectorAll('.img_deck')
 const content1 = document.querySelector('#contents_pk_deck1');
 const content2 = document.querySelector('#contents_pk_deck2');
 const content3 = document.querySelector('#contents_pk_deck3');
-const btnRemoveTrocarDeck = document.querySelector('.bkg_removePokemon');
+const btnRemoveTrocarDeck1 = document.querySelector('.bkg_removePokemon1');
+const btnRemoveTrocarDeck2 = document.querySelector('.bkg_removePokemon2');
+const btnRemoveTrocarDeck3 = document.querySelector('.bkg_removePokemon3');
 
 button.forEach((button) => {
     button.addEventListener('click', event => {
@@ -41,23 +43,119 @@ telaFundo.addEventListener('click', event => {
         content1.style.display = 'none';
         content2.style.display = 'none';
         content3.style.display = 'none';
-        btnRemoveTrocarDeck.style.display = 'none'
-
+        btnRemoveTrocarDeck1.style.display1 = 'none'
+        btnRemoveTrocarDeck2.style.display2 = 'none'
+        btnRemoveTrocarDeck3.style.display3 = 'none'
     }    
 })
 
-// construção de remoção de pokemon
-pk_deck1.forEach((imgDeck) => {
-    imgDeck.addEventListener('click', event => {
+// clicando na imagem
+pk_deck1.forEach((pk_deck1) => {
+    pk_deck1.addEventListener('click', event => {
         const classNameOfClickedElement = event.target.classList[0];
         const classeNames = ['pk_deck1']
         const verification = classeName => classeName === classNameOfClickedElement
         const condition = classeNames.some(verification)
-        const el = event.target;
-        const id = el.id;
 
         if(!condition) {
-            btnRemoveTrocarDeck.style.display = 'grid'
+            btnRemoveTrocarDeck1.style.display = 'grid';
         }
     }) 
 })
+
+pk_deck2.forEach((pk_deck2) => {
+    pk_deck2.addEventListener('click', event => {
+        const classNameOfClickedElement = event.target.classList[0];
+        const classeNames = ['pk_deck2']
+        const verification = classeName => classeName === classNameOfClickedElement
+        const condition = classeNames.some(verification)
+
+        if(!condition) {
+            btnRemoveTrocarDeck2.style.display = 'grid';
+        }
+    }) 
+})
+
+pk_deck3.forEach((pk_deck3) => {
+    pk_deck3.addEventListener('click', event => {
+        const classNameOfClickedElement = event.target.classList[0];
+        const classeNames = ['pk_deck2']
+        const verification = classeName => classeName === classNameOfClickedElement
+        const condition = classeNames.some(verification)
+
+        if(!condition) {
+            btnRemoveTrocarDeck3.style.display = 'grid';
+        }
+    }) 
+})
+
+// remção pokemon
+const removeImgDeck1 = document.querySelector('.remover1');
+const removeImgDeck2 = document.querySelector('.remover2');
+const removeImgDeck3 = document.querySelector('.remover3');
+
+removeImgDeck1.addEventListener('click', () => {
+    removerPkDeck1();
+})
+
+removeImgDeck2.addEventListener('click', () => {
+    removerPkDeck2();
+})
+
+removeImgDeck3.addEventListener('click', () => {
+    removerPkDeck3();
+})
+
+function removerPkDeck1() {
+    //btnRemoveTrocarDeck1.style.display = 'none'
+    
+    if(addDeck1[0].innerHTML != '') {
+        addDeck1[0].innerHTML = '';
+    } else if(addDeck1[1].innerHTML != '') {
+        addDeck1[1].innerHTML = '';
+    } else if(addDeck1[2].innerHTML != '') {
+        addDeck1[2].innerHTML = '';
+    } else if(addDeck1[3].innerHTML != '') {
+        addDeck1[3].innerHTML = '';
+    } else if(addDeck1[4].innerHTML != '') {
+        addDeck1[4].innerHTML = '';
+    } else if(addDeck1[5].innerHTML != '') {
+        alert('Pokmon não pode ser removido')
+    }
+}
+
+function removerPkDeck2() {
+    //btnRemoveTrocarDeck2.style.display = 'none'
+    
+    if(addDeck2[0].innerHTML != '') {
+        addDeck2[0].innerHTML = '';
+    } else if(addDeck2[1].innerHTML != '') {
+        addDeck2[1].innerHTML = '';
+    } else if(addDeck2[2].innerHTML != '') {
+        addDeck2[2].innerHTML = '';
+    } else if(addDeck2[3].innerHTML != '') {
+        addDeck2[3].innerHTML = '';
+    } else if(addDeck2[4].innerHTML != '') {
+        addDeck2[4].innerHTML = '';
+    } else if(addDeck2[5].innerHTML != '') {
+        alert('Pokmon não pode ser removido')
+    }
+}
+
+function removerPkDeck3() {
+    //btnRemoveTrocarDeck3.style.display = 'none'
+    
+    if(addDeck3[0].innerHTML != '') {
+        addDeck3[0].innerHTML = '';
+    } else if(addDeck3[1].innerHTML != '') {
+        addDeck3[1].innerHTML = '';
+    } else if(addDeck3[2].innerHTML != '') {
+        addDeck3[2].innerHTML = '';
+    } else if(addDeck3[3].innerHTML != '') {
+        addDeck3[3].innerHTML = '';
+    } else if(addDeck3[4].innerHTML != '') {
+        addDeck3[4].innerHTML = '';
+    } else if(addDeck3[5].innerHTML != '') {
+        alert('Pokmon não pode ser removido')
+    }
+}
--- front-end/js/buttonDisplay.js
+++ front-end/js/buttonDisplay.js
@@ -21,42 +21,19 @@ btnAdd.forEach((btnAdd) => {
 
         if(closeCondition){
             if(id === "add1") {
-                addInsideDeck1(num.pop(0));
+                addInsideDeck1(num.pop());
             }
     
             if(id === "add2") {
-                addInsideDeck2(num.pop(0));
+                addInsideDeck2(num.pop());
             }
     
             if(id === "add3") {
-                addInsideDeck3(num.pop(0));
+                addInsideDeck3(num.pop());
             }   
         }        
     })
 });
-
-//criação do remover e trocar ataque
-const removeImgDeck = document.querySelector('.remover');
-const trocaAtaque = document.querySelector('.trocarAtaque');
-
-removeImgDeck.addEventListener('click', () => {
-
-})
-
-trocaAtaque.addEventListener('click', () => {
-    
-})
-
-function remover() {
-    btnRemoveTrocarDeck.style.display = 'none'
-    alert('Remove Pokemon')
-}
-
-function trocarAtaque() {
-    btnRemoveTrocarDeck.style.display = 'none'
-    alert('Trocar Ataque')
-}
-
 
 let num = [];
 
@@ -71,52 +48,57 @@ card.forEach((card) => {
 
 function addInsideDeck1(id) {
     btn.style.display = 'none';
-    
+    let imagem = `<img class='img_deck' id=${id} src='img/pk/${id}.png'/>`;
+
     if(addDeck1[0].innerHTML === '') {
-        addDeck1[0].innerHTML = `<img class='img_deck' id=${id} src='img/pk/${id}.png'/>`;
+        addDeck1[0].innerHTML = imagem;
     } else if(addDeck1[1].innerHTML === '') {
-        addDeck1[1].innerHTML = `<img class='img_deck' id=${id} src='img/pk/${id}.png'/>`;
+        addDeck1[1].innerHTML = imagem;
     } else if(addDeck1[2].innerHTML === '') {
-        addDeck1[2].innerHTML = `<img class='img_deck' id=${id} src='img/pk/${id}.png'/>`;
-    } else if (addDeck1[3].innerHTML === '') {
-        addDeck1[3].innerHTML = `<img class='img_deck' id=${id} src='img/pk/${id}.png'/>`;
-    } else if (addDeck1[4].innerHTML === '') {
-        addDeck1[4].innerHTML = `<img class='img_deck' id=${id} src='img/pk/${id}.png'/>`;
-    } else if (addDeck1[5].innerHTML === '') {
-        addDeck1[5].innerHTML = `<img class='img_deck' id=${id} src='img/pk/${id}.png'/>`;
+        addDeck1[2].innerHTML = imagem;
+    } else if(addDeck1[3].innerHTML === '') {
+        addDeck1[3].innerHTML = imagem;
+    } else if(addDeck1[4].innerHTML === '') {
+        addDeck1[4].innerHTML = imagem;
+    } else if(addDeck1[5].innerHTML === '') {
+        addDeck1[5].innerHTML = imagem;
     }
 }
 
 function addInsideDeck2(id) {
     btn.style.display = 'none';
+    let imagem2 = `<img class='img_deck' id=${id} src='img/pk/${id}.png'/>`;
+
     if(addDeck2[0].innerHTML === '') {
-        addDeck2[0].innerHTML = `<img class='img_deck' id=${id} src='img/pk/${id}.png'/>`;
-    } else if(addDeck2[1].innerHTML != addDeck2[0].innerHTML) {
-        addDeck2[1].innerHTML = `<img class='img_deck' id=${id} src='img/pk/${id}.png'/>`;
+        addDeck2[0].innerHTML = imagem2;
+    } else if(addDeck2[1].innerHTML === '') {
+        addDeck2[1].innerHTML = imagem2;
     } else if(addDeck2[2].innerHTML === '') {
-        addDeck2[2].innerHTML = `<img class='img_deck' id=${id} src='img/pk/${id}.png'/>`;
-    } else if (addDeck2[3].innerHTML === '') {
-        addDeck2[3].innerHTML = `<img class='img_deck' id=${id} src='img/pk/${id}.png'/>`;
-    } else if (addDeck1[4].innerHTML === '') {
-        addDeck2[4].innerHTML = `<img class='img_deck' id=${id} src='img/pk/${id}.png'/>`;
-    } else if (addDeck2[5].innerHTML === '') {
-        addDeck2[5].innerHTML = `<img class='img_deck' id=${id} src='img/pk/${id}.png'/>`;
+        addDeck2[2].innerHTML = imagem2;
+    } else if(addDeck2[3].innerHTML === '') {
+        addDeck2[3].innerHTML = imagem2;
+    } else if(addDeck2[4].innerHTML === '') {
+        addDeck2[4].innerHTML = imagem2;
+    } else if(addDeck2[5].innerHTML === '') {
+        addDeck2[5].innerHTML = imagem2;
     }
 }
 
 function addInsideDeck3(id) {
     btn.style.display = 'none';
+    let imagem3 = `<img class='img_deck' id=${id} src='img/pk/${id}.png'/>`;
+
     if(addDeck3[0].innerHTML === '') {
-        addDeck3[0].innerHTML = `<img class='img_deck' id=${id} src='img/pk/${id}.png'/>`;
+        addDeck3[0].innerHTML = imagem3;
     } else if(addDeck3[1].innerHTML === '') {
-        addDeck3[1].innerHTML = `<img class='img_deck' id=${id} src='img/pk/${id}.png'/>`;
+        addDeck3[1].innerHTML = imagem3;
     } else if(addDeck3[2].innerHTML === '') {
-        addDeck3[2].innerHTML = `<img class='img_deck' id=${id} src='img/pk/${id}.png'/>`;
-    } else if (addDeck3[3].innerHTML === '') {
-        addDeck3[3].innerHTML = `<img class='img_deck' id=${id} src='img/pk/${id}.png'/>`;
-    } else if (addDeck3[4].innerHTML === '') {
-        addDeck3[4].innerHTML = `<img class='img_deck' id=${id} src='img/pk/${id}.png'/>`;
-    } else if (addDeck3[5].innerHTML === '') {
-        addDeck3[5].innerHTML = `<img class='img_deck' id=${id} src='img/pk/${id}.png'/>`;
+        addDeck3[2].innerHTML = imagem3;
+    } else if(addDeck3[3].innerHTML === '') {
+        addDeck3[3].innerHTML = imagem3;
+    } else if(addDeck3[4].innerHTML === '') {
+        addDeck3[4].innerHTML = imagem3;
+    } else if(addDeck3[5].innerHTML === '') {
+        addDeck3[5].innerHTML = imagem3;
     }
 }
